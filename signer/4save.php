cdc2233700db397a34241ca9b75346dc1e6c533e
John Christensen <johnawesomejr@gmail.com>
2021-11-05 19:51:57 -0700

    pdf is working great!

--- signer/4save.php
+++ signer/4save.php
@@ -30,10 +30,10 @@
         }
     </style>
 
-    <!-- <div id="screen"> -->
-    <?php //require "/var/www/html/uiImages/loading/loading.php"; 
-    ?>
-    <!-- </div> -->
+    <div id="screen">
+        <?php require "/var/www/html/uiImages/loading/loading.php";
+        ?>
+    </div>
 
 
     <?php
@@ -41,43 +41,91 @@
     $sql = <<<EOD
     SELECT * 
     FROM esignature.signers
-    WHERE signerId =?;
+    WHERE signerParentContract =?;
     EOD;
 
-    $id = $_GET['contractSigner'];
+    $id = $_GET['contractNumber'];
 
     $pdo = new PDO('mysql:host=localhost;dbname=esignature', "root", "il0veG@D");
     $stmt = $pdo->prepare($sql);
     $stmt->execute([$id]);
     $rows = $stmt->fetchAll();
     ?>
-    <pre>
-        <?php print_r($rows); ?>
-    </pre>
-
-
 
     <?php
+    $contractNotDoneSigning = FALSE;
+    foreach ($rows as $key => $value) {
+        if ($rows[$key]['signDate'] == "") {
+            $contractNotDoneSigning = TRUE;
+        }
+    }; ?>
+    <?php
+    if ($contractNotDoneSigning == FALSE) {
+
+        $signers = "";
+        foreach ($rows as $value => $id) {
+            $signatureCss = "style=\"width:300px;\" ";
+            $pathToSignature = "/var/www/html/signature" . $rows[$value]['signerImagePath'];
+            $svgImage = "<img class=sig {$signatureCss} src=\"{$pathToSignature}\">";
+            $signatureDate = $rows[$value]['signDate'];
+            $signerEmail = $rows[$value]['signerEmail'];
+            $signerTitle = $rows[$value]['signerTitle'];
+            $signatureName = $rows[$value]['signerName'];
+            $html = <<<EOD
+
+        <div style="width: 100%; border-radius: 15px; padding:30px; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); ">
+            <div align="left" style="width: 50%;float: left;">
+                <h2>{$signerTitle}</h2>
+                <h4>{$signatureName}</h4>
+                <h4>{$signerEmail}</h4>
+            </div>
+            <div align="left" style="width: 50%;float: left;">
+                <div style="width: 100%;">
+                    <img style="width:200px;" src="{$pathToSignature}">
+                    <p>Signed: {$signatureDate}</p>
+                </div>
+            </div>
+        </div>
+        EOD;
+
+            $signers = $signers . $html;
+        }
 
 
-    $signatureCss = "style=\"width:300px;\" ";
-    $pathToSignature = "/var/www/html/signature" . $rows[0]['signerImagePath'];
-    $svgImage = "<img class=sig {$signatureCss} src=\"{$pathToSignature}\">";
-    $signatureDate = $rows[0]['signDate'];
-    $signerEmail = $rows[0]['signerEmail'];
-    $signerTitle = $rows[0]['signerTitle'];
+        $sql = <<<EOD
+        SELECT contractContent, contractName
+        FROM esignature.contract
+        WHERE contractId = ?;
+        EOD;
+
+        $id = $_GET['contractNumber'];
+
+        $pdo = new PDO('mysql:host=localhost;dbname=esignature', "root", "il0veG@D");
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$id]);
+        $rows = $stmt->fetchAll();
+
+        $title = "<h1>" . $rows[0]['contractName'] . "</h1> <br>";
+        $content = $rows[0]['contractContent'];
 
 
-    require "/var/www/html/testBlog.php";
 
-    require_once __DIR__ . "/.." . '/vendor/autoload.php';
+        // require "/var/www/html/testBlog.php";
 
-    $mpdf = new \Mpdf\Mpdf();
-    $mpdf->WriteHTML($blog);
-    $mpdf->Output();
+        require_once __DIR__ . "/.." . '/vendor/autoload.php';
+        $mpdf = new \Mpdf\Mpdf();
+        $mpdf->WriteHTML($title . $content . $signers);
+        // $mpdf->Output();
+        $mpdf->Output('/var/www/html/pdfFiles/mydoc.pdf');
+    } else {
+        echo "we will email you when everyone has signed the contract.";
+    }
 
-    // $mpdf->Output('/var/www/html/pdfFiles/mydoc.pdf');
+
+
     ?>
+    <?php $urlPath = "/signer/5done.php?" . "contractNumber=" . $_GET['contractNumber'] . "&" . "contractSigner=" . $_GET['contractSigner']; ?>
+    <?php header("Location: {$urlPath}"); ?>
 
 
 
--- testBlog.php
+++ testBlog.php
@@ -3,7 +3,7 @@ $blog = <<<EOD
 <div style="width: 100%; border-radius: 15px; padding:30px; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); ">
     <div align="left" style="width: 50%;float: left;">
         <h2>{$signerTitle}</h2>
-        <h4>{$rows[0]['signerName']}</h4>
+        <h4>{$rows[1]['signerName']}</h4>
         <h4>{$signerEmail}</h4>
     </div>
     <div align="left" style="width: 50%;float: left;">
@@ -12,6 +12,5 @@ $blog = <<<EOD
             <p>Signed: {$signatureDate}</p>
         </div>
     </div>
-
 </div>
 EOD;
