4c00caef703999d1923922f7708518c1473735ae
John Christensen <johnawesomejr@gmail.com>
2021-11-08 06:49:30 -0800

    good

--- signer/5done.php
+++ signer/5done.php
@@ -1,6 +1,8 @@
 <?php
+// enviroment variables
 require "/var/www/html/.env";
 
+//top level php mailer class
 require "/var/www/html/vendor/phpmailer/phpmailer/src/PHPMailer.php";
 require "/var/www/html/vendor/phpmailer/phpmailer/src/SMTP.php";
 require "/var/www/html/vendor/phpmailer/phpmailer/src/Exception.php";
@@ -10,75 +12,100 @@ use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 $mail = new PHPMailer(true);
-?>
 
 
+//if contract emails have not been sent
 
-<?php
 $sql = <<<EOD
-SELECT * 
-FROM esignature.signers
-WHERE signerParentContract = ?;
+SELECT emailSent
+FROM esignature.contract
+WHERE contractId = ?;
 EOD;
 
 $id = $_GET['contractNumber'];
-
-$pdo = new PDO('mysql:host=localhost;dbname=esignature', "root", "il0veG@D");
+$pdo = new PDO('mysql:host=localhost;dbname=esignature', $mysqlUser, $mysqlPassword);
 $stmt = $pdo->prepare($sql);
 $stmt->execute([$id]);
-$rows = $stmt->fetchAll();
+$emailStatus = $stmt->fetchAll();
 ?>
 
 
-<?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-?>
-<h1>All done!</h1>
-<?php if ($_GET['email'] == "no") : ?>
-    <p>We will send you an email with a copy of the contract once everyone signs.</p>
-<?php else : ?>
-    <p>please download the pdf copy of the signed contract</p>
-    <?php
-    $filePath = '/var/www/html' . urldecode($_GET['downloadLink']);
-    chmod($filePath, 0777);
-    ?>
 
-    <a href="<?= "" . urldecode($_GET['downloadLink']); ?>" download>
-        DOWNLOAD PDF CONTRACT
-    </a>
+
+<?php if ($emailStatus[0]['emailSent'] < 1) : ?>
 
     <?php
-    $emailToAdresses = array();
-    foreach ($rows as $key => $value) {
-        array_push($emailToAdresses, $rows[$key]['signerEmail']);
-    }
-    $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com';
-    $mail->SMTPAuth = true;
-    $mail->SMTPSecure = "tls";
-    $mail->Port = "587";
-    $mail->Username = $googleEmail;
-    $mail->Password = $googlePassword;
-    $mail->isHTML(true);
-    $mail->Subject = 'Everyone has signed!';
-    $mail->setFrom($googleEmail);
-    $mail->Body = 'Everyone has signed the contract!<br> you can now download it <a href="http://www.google.com">here</a> or you cand save the attatchment on this email. <br>Thank you for using our e-signature service!<br>If you appreciate this service please tell oters about your expiriance! Thank you, thank you!';
-    foreach ($emailToAdresses as $key => $value) {
-        $mail->addAddress($emailToAdresses[$key]);
-    }
-    $mail->addAttachment($filePath, 'Contract.pdf');
-    $mail->send();
-    $mail->smtpClose();
+    $sql = <<<EOD
+    UPDATE esignature.contract 
+    SET emailSent=1
+    WHERE contractId=?;
+    EOD;
+
+    $id = $_GET['contractNumber'];
+
+    $pdo = new PDO('mysql:host=localhost;dbname=esignature', $mysqlUser, $mysqlPassword);
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$id]);
+    $rows = $stmt->fetchAll();
+
+
+
+
+    // sql get a list of signers
+    $sql = <<<EOD
+     SELECT * 
+     FROM esignature.signers 
+     WHERE signerParentContract=?; 
+     EOD;
+    $id = $_GET['contractNumber'];
+    $pdo = new PDO('mysql:host=localhost;dbname=esignature', $mysqlUser, $mysqlPassword);
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$id]);
+    $rows = $stmt->fetchAll();
     ?>
-    <pre>
-        <?php print_r($emailToAdresses); ?>
-    </pre>
 
 
+    <h1>All done!</h1>
+    <?php if ($_GET['email'] == "no") : ?>
+        <p>We will send you an email with a copy of the contract once everyone signs.</p>
+    <?php else : ?>
+        <p>please download the pdf copy of the signed contract</p>
+        <?php
+        $filePath = '/var/www/html' . urldecode($_GET['downloadLink']);
+        chmod($filePath, 0777);
+        ?>
 
+        <a href="<?= "" . urldecode($_GET['downloadLink']); ?>" download>
+            DOWNLOAD PDF CONTRACT
+        </a>
+
+        <?php
+        $emailToAdresses = array();
+        foreach ($rows as $key => $value) {
+            array_push($emailToAdresses, $rows[$key]['signerEmail']);
+        }
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->SMTPSecure = "tls";
+        $mail->Port = "587";
+        $mail->Username = $googleEmail;
+        $mail->Password = $googlePassword;
+        $mail->isHTML(true);
+        $mail->Subject = 'Everyone has signed!';
+        $mail->setFrom($googleEmail);
+        $mail->Body = 'Everyone has signed the contract!<br> you can now download it <a href="http://www.google.com">here</a> or you cand save the attatchment on this email. <br>Thank you for using our e-signature service!<br>If you appreciate this service please tell oters about your expiriance! Thank you, thank you!';
+        foreach ($emailToAdresses as $key => $value) {
+            $mail->addAddress($emailToAdresses[$key]);
+        }
+        $mail->addAttachment($filePath, 'Contract.pdf');
+        $mail->send();
+        $mail->smtpClose();
+        ?>
+
+    <?php endif; ?>
 <?php endif; ?>
+
 
 
 
