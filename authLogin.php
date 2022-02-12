8e89031c3e7b0bdc05e543633615b6cf5ceca17c
johnawesomejr <johnawesomejr@gmail.com>
2022-02-12 10:46:55 -0600

    working6

--- authLogin.php
+++ authLogin.php
@@ -40,15 +40,15 @@ EOD;
     if ($rows != true) {
         $errorText = "No Account Found";
 
-        echo <<<EOD
-        <form method="post" action="{$rootFolder}">
-            <input type="hidden" name="error" value="{$errorText}">
-            <input type="hidden" name="lastTypedEmail" value="{$userName}">
-            <input id="submit" type="submit" hidden>
-        </form>
-        <script>
-            document.getElementById("submit").click();
-        </script>
+echo <<<EOD
+<form method="post" action="{$rootFolder}">
+<input type="hidden" name="error" value="{$errorText}">
+<input type="hidden" name="lastTypedEmail" value="{$userName}">
+<input id="submit" type="submit" hidden>
+</form>
+<script>
+document.getElementById("submit").click();
+</script>
 EOD;
 
     } else {
