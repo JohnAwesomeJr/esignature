3dda249793eacd2c88f1f30d237c48a1a8917cab
John Christensen <johnawesomejr@gmail.com>
2022-07-15 15:26:28 -0500

    changed the PDO variable

--- authLogin.php
+++ authLogin.php
@@ -1,8 +1,8 @@
 <?php
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
-    ?>
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
 <?php
 require "/var/www/html/esignature/.env";
 session_start();
@@ -17,7 +17,7 @@ EOD;
 $userName = $_POST['userName'];
 $userPassword = $_POST['password'];
 
-$pdo = new PDO('mysql:host=localhost;dbname=esignature', $mysqlUser, $mysqlPassword);
+$pdo = new PDO("mysql:host={$mysqlIpAddress};dbname=esignature", $mysqlUser, $mysqlPassword);
 $stmt = $pdo->prepare($sql);
 $stmt->execute([$userName,]);
 $rows = $stmt->fetchAll();
@@ -30,7 +30,7 @@ $HashedPassword = hash("sha512", $_POST['password'] . $rows[0]['salt']);
 if ($userName == "" | $userPassword == "") {
     $errorText = "Please Correct User Name Or Password.";
 
-echo <<<EOD
+    echo <<<EOD
 <form method="post" action="{$rootFolder}">
 <input type="hidden" name="error" value="{$errorText}">
 <input type="hidden" name="lastTypedEmail" value="{$userName}">
@@ -44,7 +44,7 @@ EOD;
     if ($rows != true) {
         $errorText = "No Account Found";
 
-echo <<<EOD
+        echo <<<EOD
 <form method="post" action="{$rootFolder}">
 <input type="hidden" name="error" value="{$errorText}">
 <input type="hidden" name="lastTypedEmail" value="{$userName}">
@@ -54,12 +54,11 @@ echo <<<EOD
 document.getElementById("submit").click();
 </script>
 EOD;
-
     } else {
         if ($HashedPassword != $rows[0]['userPassword']) {
             $errorText = "that is not the right password for that account.";
 
-echo <<<EOD
+            echo <<<EOD
 <form method="post" action="{$rootFolder}">
     <input type="hidden" name="error" value="{$errorText}">
     <input type="hidden" name="lastTypedEmail" value="{$userName}">
@@ -69,13 +68,12 @@ echo <<<EOD
     document.getElementById("submit").click();
 </script>
 EOD;
-
         } else {
             $_SESSION['userName'] = $userName;
             $_SESSION['userId'] = $rows[0]['userId'];
             $errorText = "You are now qualified to login!";
 
-echo <<<EOD
+            echo <<<EOD
 <form method="post" action="{$rootFolder}templatesAndContracts.php">
     <input id="submit" type="submit" hidden>
 </form>
@@ -83,7 +81,6 @@ echo <<<EOD
         document.getElementById("submit").click();
 </script>
 EOD;
-
         }
     }
 }
